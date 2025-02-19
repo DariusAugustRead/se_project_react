ce03739cc9d808aa676d54b4e730a6f35a76fa56
dariu <darius.studiosuipod@gmail.com>
2025-02-19 13:43:55 -0600

    finish weather card basics

---
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -0,0 +1,14 @@
+import sunnyDay from "../../assets/sunnyDay.png";
+
+import "./WeatherCard.css";
+
+function WeatherCard() {
+  return (
+    <section className="weather-card">
+      <p className="weather-card__temp">75 &deg; F</p>
+      <img src={sunnyDay} alt="Sunny Day" className="weather-card__image" />
+    </section>
+  );
+}
+
+export default WeatherCard;
