7d7f8147a53d8bd7fd950bcfa5594eb09ac0f85c
dariu <darius.studiosuipod@gmail.com>
2025-02-21 12:33:13 -0600

    style: cards

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,11 +1,11 @@
-import "../Main/Main.css";
+import "./ItemCard.css";
 
 function ItemCard({ item }) {
   return (
-    <div className="card">
+    <li className="card">
       <h2 className="card__name">{item.name}</h2>
       <img src={item.link} alt={item.name} className="card__image" />
-    </div>
+    </li>
   );
 }
 
