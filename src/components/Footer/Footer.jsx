a8e5ba97936e6b66127ae59bb32cb33c48c382b1
dariu <darius.studiosuipod@gmail.com>
2025-02-25 15:02:13 -0600

    style: add clothes modal - video 8

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="developer__name">Developed by Stephen B. Smith</p>
+      <p className="developer__date">2025</p>
+    </footer>
+  );
+}
+
+export default Footer;
