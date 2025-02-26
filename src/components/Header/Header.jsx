061b85e0c95cb63f8727a0078daa9663c39f9308
dariu <darius.studiosuipod@gmail.com>
2025-02-26 13:08:57 -0600

    style: new garment modal & feat: open and close modal

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,14 +2,20 @@ import "./Header.css";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.svg";
 
-function Header() {
+function Header({ handleAddClick }) {
   return (
     <header className="header">
       <div className="header__left">
         <img className="header__logo" src={logo} />
         <p className="header__date-and-location">DATE, LOCATION</p>
       </div>
-      <button className="header__add-clothes-btn">+ Add clothes</button>
+      <button
+        className="header__add-clothes-btn"
+        type="button"
+        onClick={handleAddClick}
+      >
+        + Add clothes
+      </button>
       <div className="header__user-container">
         <p className="header__username">Terrence Tegegne</p>
         <img alt="Terrence Tegegne" className="header__avatar" src={avatar} />
