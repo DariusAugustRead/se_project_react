4e80896af248c8a9ad72edbc3c0e1c6e161f99c2
dariu <darius.studiosuipod@gmail.com>
2025-03-02 20:29:30 -0600

    feat: weather card is now dynamic

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,77 @@
+export const weatherOptions = [
+  {
+    day: true,
+    condition: "clear",
+    url: new URL("../assets/day/clear.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "cloudy",
+    url: new URL("../assets/day/cloudy.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "foggy",
+    url: new URL("../assets/day/foggy.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "rainy",
+    url: new URL("../assets/day/rainy.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "snowy",
+    url: new URL("../assets/day/snowy.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "stormy",
+    url: new URL("../assets/day/stormy.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "clear",
+    url: new URL("../assets/night/clear.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "cloudy",
+    url: new URL("../assets/night/cloudy.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "foggy",
+    url: new URL("../assets/night/foggy.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "rainy",
+    url: new URL("../assets/night/rainy.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "snowy",
+    url: new URL("../assets/night/snowy.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "stormy",
+    url: new URL("../assets/night/stormy.png", import.meta.url).href,
+  },
+];
+
+export const defaultWeatherOptions = {
+  day: {
+    day: true,
+    url: new URL("../assets/day/default.png", import.meta.url).href,
+  },
+  night: {
+    day: false,
+    url: new URL("../assets/night/default.png", import.meta.url).href,
+  },
+};
+
 export const defaultClothingItems = [
   {
     _id: 0,
