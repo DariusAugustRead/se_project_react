6094cc6552c423065459e30662dbd4d864cd6f79
dariu <darius.studiosuipod@gmail.com>
2025-03-26 14:01:10 -0500

    ?

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,4 +6,42 @@ function getItems() {
   });
 }
 
-export { getItems };
+function postItems() {
+  return fetch(
+    `${baseUrl}/items`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        name: items.name,
+        imageUrl: items.imageUrl,
+        weather: items.weather,
+      }),
+      headers: {
+        "Content-Type": application / json,
+      },
+    }.then((res) => {
+      return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+    })
+  );
+}
+
+function deleteItems() {
+  return fetch(
+    `${baseUrl}/items`,
+    {
+      method: "DELETE",
+      body: JSON.stringify({
+        name: items.name,
+        imageUrl: items.imageUrl,
+        weather: items.weather,
+      }),
+      headers: {
+        "Content-Type": application / json,
+      },
+    }.then((res) => {
+      return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+    })
+  );
+}
+
+export { getItems, postItems };
