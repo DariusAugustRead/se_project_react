d5cd4c24a9ff9a7426ca9a8e25eb553e9a4d33ec
dariu <darius.studiosuipod@gmail.com>
2025-04-13 19:19:37 -0500

    finish: project matches figma design and project description

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -5,8 +5,14 @@ import avatar from "../../assets/avatar.svg";
 function SideBar() {
   return (
     <div className="sidebar">
-      <img className="sidebar__avatar" src={avatar} alt="Default avatar" />
-      <p className="sidebar__username">Terrence Tegegne</p>
+      <div className="sidebar__user_info-display">
+        <img className="sidebar__avatar" src={avatar} alt="Default avatar" />
+        <p className="sidebar__username">Terrence Tegegne</p>
+      </div>
+      <div className="sidebar__user_info-change">
+        <button className="sidebar__change_profile">Change profile data</button>
+        <button className="sidebar__log-out">Log out</button>
+      </div>
     </div>
   );
 }
