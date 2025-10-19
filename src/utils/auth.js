3d83c2402b224a97ce408b424ba26a3a2181f884
dariu <darius.studiosuipod@gmail.com>
2025-10-19 09:54:52 -0500

    baseurl update

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,7 @@
-const baseUrl = "http://localhost:3001";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.wtwr.home-ip.hu"
+    : "http://localhost:3001";
 
 import { handleServerResponse } from "./api.js";
 
